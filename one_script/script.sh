022f90cb1df53fa9712bc1698b0663d433746ff0
Marius Beuerle <mbeuerle@uni-bremen.de>
2024-01-04 09:37:50 +0100

    Results

--- one_script/script.sh
+++ one_script/script.sh
@@ -1,1 +1,10 @@
 make
+./randomBond --open true --p 0.10913 --temp 0.9 --step 0.04 --up true --nie 200 --nis 10 --nit 500000 --nw 100 --nl 16 --nrt 1 --L 256 --folder 0.10913
+./randomBond --open true --p 0.10914 --temp 0.9 --step 0.04 --up true --nie 200 --nis 10 --nit 500000 --nw 100 --nl 16 --nrt 1 --L 256 --folder 0.10914
+./randomBond --open true --p 0.10915 --temp 0.9 --step 0.04 --up true --nie 200 --nis 10 --nit 500000 --nw 100 --nl 16 --nrt 1 --L 256 --folder 0.10915
+./randomBond --open true --p 0.10916 --temp 0.9 --step 0.04 --up true --nie 200 --nis 10 --nit 500000 --nw 100 --nl 16 --nrt 1 --L 256 --folder 0.10916
+./randomBond --open true --p 0.10917 --temp 0.9 --step 0.04 --up true --nie 200 --nis 10 --nit 500000 --nw 100 --nl 16 --nrt 1 --L 256 --folder 0.10917
+./randomBond --open true --p 0.10918 --temp 0.9 --step 0.04 --up true --nie 200 --nis 10 --nit 500000 --nw 100 --nl 16 --nrt 1 --L 256 --folder 0.10918
+./randomBond --open true --p 0.10919 --temp 0.9 --step 0.04 --up true --nie 200 --nis 10 --nit 500000 --nw 100 --nl 16 --nrt 1 --L 256 --folder 0.10919
+./randomBond --open true --p 0.1092 --temp 0.9 --step 0.04 --up true --nie 200 --nis 10 --nit 500000 --nw 100 --nl 16 --nrt 1 --L 256 --folder 0.1092 
+./randomBond --open true --p 0.10921 --temp 0.9 --step 0.04 --up true --nie 200 --nis 10 --nit 500000 --nw 100 --nl 16 --nrt 1 --L 256 --folder 0.10921

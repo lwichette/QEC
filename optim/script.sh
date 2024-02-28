1366bba23fb7386a505281fbafaa43221bf1e6ee
Marius Beuerle <mbeuerle@uni-bremen.de>
2024-02-28 09:17:25 +0100

    Results periodic boundary

--- optim/script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./cuIsing --X 512 --Y 512 --XSL 128 --YSL 128 --temp 1.0 --step 0.1 --nie 500 --nw 20000000 --nit 1000 --up 0 --ndev 1 --prob 0.06 --out
-./cuIsing --X 512 --Y 512 --XSL 128 --YSL 128 --temp 1.0 --step 0.1 --nie 500 --nw 20000000 --nit 1000 --up 1 --ndev 1 --prob 0.06 --out

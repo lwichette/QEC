3398aef2cef15adf8c10aac6b10061078cac4324
Linnea Grans-Samuelsson <linneag@microsoft.com>
2024-08-15 20:39:34 -0700

    first runs with new error strings

--- WangLandau/run_batch.sh
+++ WangLandau/run_batch.sh
@@ -2,31 +2,35 @@
 
 SECONDS=0
 
-xval=8
-yval=8
+xval=10
+yval=10
 
-for seed in {100..130}
+#Note: we need to use fewer intervals, at least at low system size -- keep getting issues with check_energy_ranges for 8 by 8
+intervals=5
+beta=0.0001
+
+for seed in {113..114}
 do
 echo $seed
 
-./prerun -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i 20 -e "I" -b 0
+./prerun_-10 -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i $intervals -e "I" -b 0
 wait
-./wl -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b 0.000001 -i 20 -w 8 -o 0.25 -s $seed -e "I" -t 0 -h $seed
-wait
-
-./prerun -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i 20 -e "X" -b 0
-wait
-./wl -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b 0.000001 -i 20 -w 8 -o 0.25 -s $seed -e "X" -t 0 -h $seed
+./wl_-10 -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "I" -t 0 -h $seed
 wait
 
-./prerun -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i 20 -e "Z" -b 0
+./prerun_-10 -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i $intervals -e "X" -b 0
 wait
-./wl -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b 0.000001 -i 20 -w 8 -o 0.25 -s $seed -e "Z" -t 0 -h $seed
+./wl_-10 -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "X" -t 0 -h $seed
 wait
 
-./prerun -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i 20 -e "Y" -b 0
+./prerun_-10 -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i $intervals -e "Z" -b 0
 wait
-./wl -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b 0.000001 -i 20 -w 8 -o 0.25 -s $seed -e "Y" -t 0 -h $seed
+./wl_-10 -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "Z" -t 0 -h $seed
+wait
+
+./prerun_-10 -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i $intervals -e "Y" -b 0
+wait
+./wl_-10 -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "Y" -t 0 -h $seed
 wait
 echo "Done"
 

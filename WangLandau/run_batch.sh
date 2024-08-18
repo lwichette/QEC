c6f1b5e3f29a89a74b7502bc849904f609d17601
Linnea Grans-Samuelsson <linneag@microsoft.com>
2024-08-17 20:34:20 -0700

    looping over probabilities

--- WangLandau/run_batch.sh
+++ WangLandau/run_batch.sh
@@ -5,45 +5,54 @@ SECONDS=0
 #Note: should make sure we have "fresh" histograms from the prerun, probably?
 beta=0.0001
 
-for seed in {100..500}
+for probability in {0.02,0.04,0.06,0.08,0.1,0.12}
 do
 
-for size in {12,14}
+for seed in {143..200}
+do
+
+for size in {4,6}
 do
 
 xval=$size
 yval=$size
 
+
+intervals=10
+
+iterations=10000
+
 #Might adjust later
-if [[ $size > 10 ]]; then
-intervals=20
-else
-intervals=5
-fi
+# if [[ $size > 10 ]]; then
+# intervals=20
+# fi
+
+
 
 echo $seed
 
-./prerun_-10 -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i $intervals -e "I" -b 0
+./prerun_-10 -x $xval -y $yval -p $probability -n $iterations -l 10 -w 16 -s $seed -i $intervals -e "I" -b 0
 wait
-./wl_-10 -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "I" -t 0 -h $seed
-wait
-
-./prerun_-10 -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i $intervals -e "X" -b 0
-wait
-./wl_-10 -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "X" -t 0 -h $seed
+./wl_-10 -x $xval -y $yval -n $iterations -p $probability -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "I" -t 0 -h $seed
 wait
 
-./prerun_-10 -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i $intervals -e "Z" -b 0
+./prerun_-10 -x $xval -y $yval -p $probability -n $iterations -l 10 -w 16 -s $seed -i $intervals -e "X" -b 0
 wait
-./wl_-10 -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "Z" -t 0 -h $seed
+./wl_-10 -x $xval -y $yval -n $iterations -p $probability -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "X" -t 0 -h $seed
 wait
 
-./prerun_-10 -x $xval -y $yval -p 0.107 -n 100000 -l 10 -w 16 -s $seed -i $intervals -e "Y" -b 0
+./prerun_-10 -x $xval -y $yval -p $probability -n $iterations -l 10 -w 16 -s $seed -i $intervals -e "Z" -b 0
 wait
-./wl_-10 -x $xval -y $yval -n 100000 -p 0.107 -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "Y" -t 0 -h $seed
+./wl_-10 -x $xval -y $yval -n $iterations -p $probability -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "Z" -t 0 -h $seed
+wait
+
+./prerun_-10 -x $xval -y $yval -p $probability -n $iterations -l 10 -w 16 -s $seed -i $intervals -e "Y" -b 0
+wait
+./wl_-10 -x $xval -y $yval -n $iterations -p $probability -a 0.8 -b $beta -i $intervals -w 8 -o 0.25 -s $seed -e "Y" -t 0 -h $seed
 wait
 echo "Done"
 
+done
 done
 done
 duration=$SECONDS

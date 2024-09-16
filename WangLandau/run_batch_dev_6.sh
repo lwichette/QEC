77aaf206cb2278a6c33a2d301ff57ca35671bb91
Marius <mbeuerle@uni-bremen.de>
2024-09-16 14:19:27 +0200

    Results from cluster

---
+++ WangLandau/run_batch_dev_6.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+LOGFILE="script.log"
+exec > >(tee -a "$LOGFILE") 2>&1
+
+SECONDS=0
+
+alpha=0.8
+
+beta=0.000001
+
+walker_wl=8
+
+overlap_wl=0.25
+
+seed_hist=1
+
+seed_run=1000
+
+num_interactions=1000
+
+replica_exchange_steps=50
+
+boundary_type=0
+
+intervals_wl=10
+
+iterations=1000
+
+time_limit=820
+
+for probability in 0.08 0.09 0.1 0.11 0.12
+  do
+    for size in 6 
+    do
+      xval=$size
+      yval=$size
+      for error_type in X Y Z I
+      do
+        ./prerun_-10 -x $xval -y $yval -p $probability -n $iterations -l 100 -w 100 -s $seed_hist -i 20 -e "$error_type" -b $boundary_type -r $num_interactions
+
+
+        timeout $time_limit ./wl_-10 -x $xval -y $yval -n $iterations -p $probability -a $alpha -b $beta -i $intervals_wl -w $walker_wl -o $overlap_wl -s $seed_run -e "$error_type" -t $boundary_type -h $seed_hist -r $num_interactions -c $replica_exchange_steps
+        if [ $? -eq 124 ]; then
+            echo "wl_-10 timed out after $time_limit seconds."
+        fi
+
+        seed_run=$(($seed_run + 1))
+
+        echo "Done with size $size, probability $probability, error type $error_type"
+      done
+    done
+done
+
+duration=$SECONDS
+echo "Total runtime: $((duration / 60)) minutes and $((duration % 60)) seconds."
+~                                                                                 

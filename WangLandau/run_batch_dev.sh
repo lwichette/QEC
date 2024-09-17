27d2989782940537b7c2bf8b69c703bb894e85fa
Marius <mbeuerle@uni-bremen.de>
2024-09-17 16:04:00 +0200

    Cluster changes

--- WangLandau/run_batch_dev.sh
+++ WangLandau/run_batch_dev.sh
@@ -13,34 +13,31 @@ walker_wl=8
 
 overlap_wl=0.25
 
-seed_hist=1500
+seed_hist=1
 
 seed_run=1000
 
-num_interactions=1000
+num_interactions=2000
 
 replica_exchange_steps=50
 
 boundary_type=0
 
-intervals_wl=10
+intervals_wl=15
 
 iterations=1000
 
-time_limit=820
+time_limit=1000
 
-for probability in 0.08 0.09 0.1 0.11 0.12
+for probability in 0.1 0.11 0.12
   do
-    for size in 4 6 8 10
+    for size in 10
     do
       xval=$size
       yval=$size
-      for error_type in X Y Z
+      for error_type in I X Y Z
       do
-        timeout $time_limit ./prerun_-10 -x $xval -y $yval -p $probability -n $iterations -l 100 -w 100 -s $seed_hist -i 20 -e "$error_type" -b $boundary_type -r $num_interactions
-        if [ $? -eq 124 ]; then
-            echo "prerun timed out after $time_limit seconds."
-        fi
+        $time_limit ./prerun_-10 -x $xval -y $yval -p $probability -n $iterations -l 100 -w 100 -s $seed_hist -i 20 -e "$error_type" -b $boundary_type -r $num_interactions
 
         timeout $time_limit ./wl_-10 -x $xval -y $yval -n $iterations -p $probability -a $alpha -b $beta -i $intervals_wl -w $walker_wl -o $overlap_wl -s $seed_run -e "$error_type" -t $boundary_type -h $seed_hist -r $num_interactions -c $replica_exchange_steps
         if [ $? -eq 124 ]; then
@@ -51,7 +48,7 @@ for probability in 0.08 0.09 0.1 0.11 0.12
 
         echo "Done with size $size, probability $probability, error type $error_type"
 
-        rm -r init/*
+        rm -rf init/*
       done
     done
 done

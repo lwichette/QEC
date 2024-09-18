e8b4000d9ad11972a94e897333c9bac956200854
lpwichette <lpwichette@dfki.de>
2024-09-18 15:21:25 +0200

    Merge branch 'main' of https://git.hb.dfki.de/quantum/quantum-error-correction/statmech/qec into main

--- WangLandau/main_eight_vertex.cu
+++ WangLandau/main_eight_vertex.cu
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
     float prob_x_err = 0;
     float prob_y_err = 0;
     float prob_z_err = 0;
-    float overlap = 0;
+    float overlap_decimal = 0;
     float alpha = 0;
     float beta = 0;
     float error_mean = 0;
@@ -121,7 +121,7 @@ int main(int argc, char **argv)
             num_iterations = atoi(optarg);
             break;
         case 'o':
-            overlap = atof(optarg);
+            overlap_decimal = atof(optarg);
             break;
         case 'p':
             seed_hist = atoi(optarg);
@@ -182,17 +182,76 @@ int main(int argc, char **argv)
         total_len_energy_spectrum += energy_spectrum.size();
         h_len_energy_spectrum.push_back(energy_spectrum.size());
     }
+    int *d_offset_energy_spectrum, *d_len_energy_spectrum;
+    CHECK_CUDA(cudaMalloc(&d_offset_energy_spectrum, num_interactions * sizeof(*d_offset_energy_spectrum)));
+    CHECK_CUDA(cudaMemcpy(d_offset_energy_spectrum, h_offset_energy_spectrum.data(), num_interactions * sizeof(*d_offset_energy_spectrum), cudaMemcpyHostToDevice));
+    CHECK_CUDA(cudaMalloc(&d_len_energy_spectrum, num_interactions * sizeof(*d_len_energy_spectrum)));
+    CHECK_CUDA(cudaMemcpy(d_len_energy_spectrum, h_len_energy_spectrum.data(), num_interactions * sizeof(*d_len_energy_spectrum), cudaMemcpyHostToDevice));
 
-    // for (int i = 0; i < num_interactions; i++)
-    // {
-    //     std::cout << E_min[i] << " " << E_max[i] << std::endl;
-    //     for (int j = 0; j < h_len_energy_spectrum[i]; j++)
-    //     {
-    //         std::cout << static_cast<int>(h_expected_energy_spectrum[j]) << std::endl;
-    //     }
-    // }
+    // Generate intervals for all different energy spectrums
+    std::vector<int> h_end_int;
+    std::vector<int> h_start_int;
+    std::vector<int> len_histogram_int;
+    std::vector<int> len_interval_int;
 
-    // f Factors for each walker
+    long long total_len_histogram = 0;
+
+    for (int i = 0; i < num_interactions; i++)
+    {
+        IntervalResult run_result = generate_intervals(
+            E_min[i], E_max[i], num_intervals,
+            walker_per_interval, overlap_decimal);
+
+        h_end_int.insert(h_end_int.end(), run_result.h_end.begin(),
+                         run_result.h_end.end());
+
+        h_start_int.insert(h_start_int.end(), run_result.h_start.begin(),
+                           run_result.h_start.end());
+
+        len_histogram_int.push_back(run_result.len_histogram_over_all_walkers);
+        len_interval_int.push_back(run_result.len_interval);
+        total_len_histogram += run_result.len_histogram_over_all_walkers;
+    }
+
+    int *d_len_histograms;
+    CHECK_CUDA(cudaMalloc(&d_len_histograms, num_interactions * sizeof(*d_len_histograms)))
+    CHECK_CUDA(cudaMemcpy(d_len_histograms, len_histogram_int.data(), num_interactions * sizeof(*d_len_histograms), cudaMemcpyHostToDevice));
+
+    // Start end energies of the intervals
+    int *d_start, *d_end;
+    CHECK_CUDA(cudaMalloc(&d_start, total_intervals * sizeof(*d_start)));
+    CHECK_CUDA(cudaMalloc(&d_end, total_intervals * sizeof(*d_end)));
+    CHECK_CUDA(cudaMemcpy(d_start, h_start_int.data(), total_intervals * sizeof(*d_start), cudaMemcpyHostToDevice));
+    CHECK_CUDA(cudaMemcpy(d_end, h_end_int.data(), total_intervals * sizeof(*d_start), cudaMemcpyHostToDevice));
+
+    // Histogramm and G array
+    unsigned long long *d_H;
+    CHECK_CUDA(cudaMalloc(&d_H, total_len_histogram * sizeof(*d_H)));
+    CHECK_CUDA(cudaMemset(d_H, 0, total_len_histogram * sizeof(*d_H)));
+
+    double *d_logG;
+    CHECK_CUDA(cudaMalloc(&d_logG, total_len_histogram * sizeof(*d_logG)));
+    CHECK_CUDA(cudaMemset(d_logG, 0, total_len_histogram * sizeof(*d_logG)));
+
+    std::vector<long long> h_offset_shared_log_G;
+    int size_shared_log_G = 0;
+    for (int i = 0; i < num_interactions; i++)
+    {
+        for (int j = 0; j < num_intervals; j++)
+        {
+            h_offset_shared_log_G.push_back(size_shared_log_G);
+            size_shared_log_G += (h_end_int[i * num_intervals + j] - h_start_int[i * num_intervals + j] + 1);
+        }
+    }
+
+    double *d_shared_logG;
+    CHECK_CUDA(cudaMalloc(&d_shared_logG, size_shared_log_G * sizeof(*d_shared_logG)));
+    CHECK_CUDA(cudaMemset(d_shared_logG, 0, size_shared_log_G * sizeof(*d_shared_logG)));
+
+    long long *d_offset_shared_logG;
+    CHECK_CUDA(cudaMalloc(&d_offset_shared_logG, total_intervals * sizeof(*d_offset_shared_logG)));
+    CHECK_CUDA(cudaMemcpy(d_offset_shared_logG, h_offset_shared_log_G.data(), total_intervals * sizeof(*d_offset_shared_logG), cudaMemcpyHostToDevice));
+
     std::vector<double> h_factor(total_walker, exp(1.0));
 
     double *d_factor;
